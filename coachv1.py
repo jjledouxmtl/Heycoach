b7e93270d600fd82cf43a7692147608384cb2c4e
jjledouxmtl <jjledoux@hotmail.com>
2018-09-28 20:51:14 -0400

    Cleanup and comments

--- coachv1.py
+++ coachv1.py
@@ -1,18 +1,22 @@
 import pygame
 
+#Currently set to size of court.png file
 screen = pygame.display.set_mode((826,460))
 
+#Initialize variables
 draw_on = False
 last_pos = (0,0)
 color = (0,0,255)
-radius = 4
+radius = 3
 
 pygame.init()
 
+#Set background image of court
 bg = pygame.image.load("court.png")
 screen.blit(bg, (0,0))
 pygame.display.update()
 
+#Function to draw the lines
 def roundline(srf, color, start, end, radius=1):
     dx = end[0]-start[0]
     dy = end[1]-start[1]
@@ -22,6 +26,7 @@ def roundline(srf, color, start, end, radius=1):
         y = int( start[1]+float(i)/distance*dy)
         pygame.display.update(pygame.draw.circle(srf, color, (x,y), radius))
 
+#Pygame loop
 try:
     while True:
         e= pygame.event.wait()
@@ -37,9 +42,6 @@ try:
                 pygame.display.update(pygame.draw.circle(screen, color, e.pos, radius))
                 roundline(screen, color, e.pos, last_pos, radius)
             last_pos = e.pos
-
-#        screen.blit(bg, (0,0))
-#        pygame.display.update()
 
 except StopIteration:
     pass
