e108544733a850a1285ddd3c50618e8481d4cc4a
jjledouxmtl <jjledoux@hotmail.com>
2018-10-04 22:06:03 -0400

    added temp.png saving

--- coachv2.py
+++ coachv2.py
@@ -1,4 +1,4 @@
-import pygame, sys
+import pygame, sys, os
 
 #Currently set to size of court.png file
 screen = pygame.display.set_mode((1239,690))
@@ -13,11 +13,11 @@ pygame.init()
 
 button_green = pygame.Rect(585, 30, 30, 30)
 button_red = pygame.Rect(643, 30, 30, 30)
+image_dft = "court.png"
 
-
-def resetApp():
+def resetApp(image):
 #Set background image of court
-    bg = pygame.image.load("court.png")
+    bg = pygame.image.load(image)
     screen.blit(bg, (0,0))
 #Setting buttons
     pygame.draw.rect(screen, [0,255,0], button_green)
@@ -34,26 +34,29 @@ def roundline(srf, color, start, end, radius=1):
         y = int( start[1]+float(i)/distance*dy)
         pygame.display.update(pygame.draw.circle(srf, color, (x,y), radius))
 
-resetApp()
+resetApp(image_dft)
 
 #Pygame loop
 try:
     while True:
         e= pygame.event.wait()
         if e.type == pygame.QUIT:
-            raise StopIteration
+           os.remove("temp.png")
+           raise StopIteration
         if e.type == pygame.MOUSEBUTTONDOWN:
-            new_pos = pygame.mouse.get_pos()
-            e.pos = new_pos
             if button_green.collidepoint(e.pos):
-                resetApp()
+                resetApp(image_dft)
             elif button_red.collidepoint(e.pos):
+                os.remove("temp.png")
                 raise StopIteration
             else:
+                if os.path.isfile("temp.png"):
+                    resetApp("temp.png")
                 pygame.draw.circle(screen, color, e.pos, radius)
                 draw_on = True
         if e.type == pygame.MOUSEBUTTONUP:
             draw_on = False
+            pygame.image.save(screen, "temp.png")
         if e.type == pygame.MOUSEMOTION:
             if draw_on:
                 pygame.display.update(pygame.draw.circle(screen, color, e.pos, radius))
